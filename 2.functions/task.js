1fa65f3fd38e3b3838a10b563377c341a63e748c
jana <janakolesnikova6293@gmail.com>
2022-07-13 01:49:31 +0300

    Решение 2 первых задач из 3х

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0],
+      max = arr[0],
+      sum = 0,
+      avg = 0;
 
-  // Ваш код
+  for (let i in arr) {
+    if (min > arr[i]) min = arr[i];
+    if (max < arr[i]) max = arr[i];
+    sum += arr[i];
+  }
+
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+function worker(arr) {
+  let sum = [];
+  let sumTemporary;
+
+  for (let i in arr) {
+    sumTemporary = 0;
+    for (let j in arr[i]) {
+      sumTemporary += arr[i][j];
+    }
+    sum.push(sumTemporary);
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
+  let arrSum = func(arrOfArr);
 
-  // Ваш кода
-  // for ...
-  
+  for (let i in arrSum) {
+    if (max < arrSum[i]) max = arrSum[i];
+  }
+
   return max;
 }
 
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0],
+      min = arr[0];
+
+  arr.flat(); // делает из массива массивов один массив
+  for (let i in arr) {
+    if (max < arr[i]) max = arr[i];
+    if (min > arr[i]) min = arr[i];
+  }
+
+  return Math.abs(max - min);
 }
