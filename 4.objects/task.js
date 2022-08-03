f3440ab51f28652df7d4b06b9a70402badb3dcc5
jana <janakolesnikova6293@gmail.com>
2022-08-03 22:32:34 +0400

    Решение задачи по объектам

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+const student1 = new Student('Roman', 'male', 20);
+const student2 = new Student('Alex', 'male', 18);
+const student3 = new Student('Kate', 'female', 21);
+const student4 = new Student('Jane', 'female', 18);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...args) {
+  if(this.marks === undefined){
+    this.marks = [...args];
+    } else {
+      this.marks.push(...args);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+
+  for (let item of this.marks) {
+    sum += item;
+  }
+
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
