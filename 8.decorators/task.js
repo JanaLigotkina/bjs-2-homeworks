dc94aa7372a23da777f9e7fdcd99f4e4954b09fb
jana <janakolesnikova6293@gmail.com>
2022-09-03 22:16:47 +0400

    Решение задач по декораторам

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+  let cacheKeys = Object.keys(cache);
+
+  return (...args) => {
+    const hash = args.join(',');
+    if (hash in cache) {
+      console.log(`Из кэша: ${cache[hash]}`);
+      return "Из кэша: " + cache[hash];
+    }
+
+    const result = func(...args);
+    cache[hash] = result;
+    if (cacheKeys.length > 5) {
+      delete cache[cacheKeys[0]];
+    }
+
+    console.log(`Вычисляем: ${result}`);
+    return "Вычисляем: " + result; ;
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
 
-function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if(timeoutId) {
+      clearTimeout(timeoutId);
+    } else {
+      console.log(func(...args));
+      wrapper.count++;
+    }
+
+    timeoutId = setTimeout(() => {
+      console.log(func(...args));
+      wrapper.count++;
+    }, delay);
+  }
+  return wrapper;
 }
